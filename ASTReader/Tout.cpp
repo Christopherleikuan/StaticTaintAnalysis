c3e883508984a981499a524b8b6d0a95c717a12e
Christopherleikuan <1434262493@qq.com>
2016-07-22 10:01:21 +0800

    Update Tout.cpp

--- ASTReader/Tout.cpp
+++ ASTReader/Tout.cpp
@@ -1,205 +1,60 @@
-#include"Tout.h"
-Ttable::Ttable()
-{
-	head = NULL;
-}
-Ttable::~Ttable()
-{
-	Node*p = NULL;
-	while (p == head)
-	{
-		p = head;
-		while (p != NULL)
-		{
+#include<stdio.h>
+#include<iostream>
+#include<string>
+#include"AST.h"
+#include"tinyxml.h"
+#include"tinystr.h"
+using namespace std;
+#define TTYPE_ARRAY 1
+#define TTYPE_DIV_MOD 2
+#define TTYPE_LOOP_BOUND 3
+#define TTYPE_MEM 4
+#define TTYPE_POINTER_NULL_SET 5
+#define TTYPE_RECURSIVE_CALL 6
 
-			p = p->next;
-		}
-		delete p;
-	}
-}
-
-bool Ttable::CreateXmlFile(Node*p, char*file)
+class XYJ_table
 {
-	//创建一个XML的文档对象。
-	TiXmlDocument *myDocument = new TiXmlDocument();
-	//创建一个根元素并连接。
-	TiXmlElement *RootElement = new TiXmlElement("Taints");
-	myDocument->LinkEndChild(RootElement);
-	//创建一个Person元素并连接。
-	TiXmlElement *PersonElement = new TiXmlElement("Taint");
-	RootElement->LinkEndChild(PersonElement);
-	//设置Person元素的属性。
-	//PersonElement->SetAttribute("type", p->Type);
-	//创建name元素、age元素并连接。
-	TiXmlElement *NameElement = new TiXmlElement("name");
-	TiXmlElement *AgeElement = new TiXmlElement("line");
-	PersonElement->LinkEndChild(NameElement);
-	PersonElement->LinkEndChild(AgeElement);
-	//设置name元素和age元素的内容并连接。
-	TiXmlText *NameContent = new TiXmlText(p->Tname);
-	TiXmlText *AgeContent = new TiXmlText(p->line);
-	NameElement->LinkEndChild(NameContent);
-	AgeElement->LinkEndChild(AgeContent);
-	//保存到文件
-	int i;
+public:
+	FunctionDecl* fd;
+	ASTContext* astc;
+	XYJ_table(){}
+	XYJ_table(FunctionDecl* f, ASTContext* as)
+	{
+		fd = f;
+		astc = as;
+	}
+};
 
-	myDocument->SaveFile(file);
-	return 1;
-}
 
-void Ttable::outTtable()
+class Ttable
 {
-	
-}
-bool Ttable::insert(string&  line, string& function, int TYPE )
-{
-	Node *t = makeTnode(line,function,TYPE);
-	if (head == NULL)
+public:
+	struct Node
 	{
-		head = t;
-		head->next = NULL;
-	}
-	else
-	{
-		t->next = head->next;
-		head->next = t;
-	}
-	return true;
-}
-Ttable::Node* Ttable::makeTnode(string&  line, string& function, int TYPE)
-{
+		//char*Tname;
+		char*Tfile;
+		char*Tfunction;
+		char*line;
+		char*TYPE;
+		Node * next;
+	}node;
+	Ttable();
+	~Ttable();
+	void make_node(const VarDecl *p);
+	//	bool insert(const VarDecl *p,  int line,string function);
+	//	bool insert(const VarDecl *p,string line,string function,int TYPE);
+	bool insert(string& line, string& function, int TYPE);
+	void outTtable();
+	bool CreateXmlFile(Node*p, char*file);
+	VarDecl * serach(const VarDecl * p);
+	void listout();
+	void XMLout();
 
-	int num = line.find(':', 2);
-	string test_line;
-	test_line.insert(0, line, num + 1, 1);
-	cout << test_line << endl;
-	string test_file;
-	test_file.insert(0, line, 0, num);
-	cout << test_file << endl;
-//	Node * t = NULL;
-//	t = exchange(p, line, function);
-	//将行号转换为char类型
-	Node * t = new Node;
-	t->line = new char[50];
-//	sprintf(t->line, "%d", line);
-	string str_line = test_line;
-	str_line.copy(t->line,str_line.length(),0);
-	*(t->line + str_line.length())= '\0';
-	cout<<t->line<<"t->line"<<endl;
-	//将名字转换为char类型
-	//cout << p->getQualifiedNameAsString().data()<<" 23333" << endl;
-	//cout <<(char*) p->getQualifiedNameAsString().data() << " 1111" << endl;
-	t->Tname = new char[50];
-	t->Tname = NULL;
-//	string test = (char*)p->getQualifiedNameAsString().data();
-	//t->Tname = (char*)test.data();
-//	test.copy(t->Tname, test.length(), 0);
-//	*(t->Tname + test.length()) = '\0';
-//	cout << t->Tname << " t->Tname" << endl;
-	//t->Tname = (char*)p->getQualifiedNameAsString().data();
-	//cout <<t->Tname <<" 344444" << endl;
-	/*为什么不用getDeclName函数获取变量的名字呢,huozhe shi p->getNameAsString*/
-	//将函数名字转换为char
-	t->Tfunction = new char[100];
-	string funtion_name = function;
-	//cout << funtion_name << 111212313 << endl;
-	//t->Tfunction = (char*)funtion_name.data();
-	funtion_name.copy(t->Tfunction, funtion_name.length(), 0);
-	*(t->Tfunction + funtion_name.length())= '\0';
-	cout << t->Tfunction << " Tfunction" << endl;
-	//通过clang得到了该变量所在的文件名。
-//	t->Tfile = (char*)p->getASTContext().getSourceManager().getFilename(p->getSourceRange().getBegin()).data();
-	t->Tfile = new char[500];
-	string Tfile_name = test_file;
-	Tfile_name.copy(t->Tfile, Tfile_name.length(), 0);
-	*(t->Tfile + Tfile_name.length()) = '\0';
-	cout << t->Tfile << " Tfile" << endl;
-	
-	
-	t->TYPE = new char[100];
-	if (TYPE == 1)
-	{
-		string Type = "TTYPE_ARRAY";
-		Type.copy(t->TYPE, Type.length(), 0);
-		*(t->TYPE + Type.length()) = '\0';
-	}
-	else if (TYPE == 2)
-	{
-		string Type = "TTYPE_DIV_MOD";
-		Type.copy(t->TYPE, Type.length(), 0);
-		*(t->TYPE + Type.length()) = '\0';
-	}
-	else if (TYPE==3)
-	{
-		string Type = "TTYPE_LOOP_BOUND";
-		Type.copy(t->TYPE, Type.length(), 0);
-		*(t->TYPE + Type.length()) = '\0';
-	}
-	else if (TYPE == 4)
-	{
-		string Type = "TTYPE_MEM";
-		Type.copy(t->TYPE, Type.length(), 0);
-		*(t->TYPE + Type.length()) = '\0';
-	}
-	else if (TYPE==5)
-	{
-		string Type = "TTYPE_POINTER_NULL_SET";
-		Type.copy(t->TYPE, Type.length(), 0);
-		*(t->TYPE + Type.length()) = '\0';
-	}
-	else if (TYPE == 6)
-	{
-		string Type = "TTYPE_RECURSIVE_CALL";
-		Type.copy(t->TYPE, Type.length(), 0);
-		*(t->TYPE + Type.length()) = '\0';
-	}
+private:
 
-	return t;
-	
-}
-Ttable::Node* Ttable::exchange(string&  line, string& function, int TYPE)
-{
-	//将行号转换为char类型
-	Node * t = new Node;
-	t->line = new char[50];
-	sprintf(t->line, "%d", line);
-	//将名字转换为char类型
-//	t->Tname = (char*)p->getQualifiedNameAsString().data();
-	/*为什么不用getDeclName函数获取变量的名字呢,huozhe shi p->getNameAsString*/
-	//将函数名字转换为char
-	t->Tfunction = (char*)function.data();
-	//通过clang得到了该变量所在的文件名。
-//	t->Tfile = (char*)p->getASTContext().getSourceManager().getFilename(p->getSourceRange().getBegin()).data();
-	return t;
+	Node * head;
 
-}
-void Ttable::listout()
-{
-	Node * t = NULL;
-	t = head;
-	while (t != NULL)
-	{
-		cout << t->Tname << endl;
-		cout << t->line << endl;
-		cout << t->Tfunction << endl;
-		cout << t->Tfile << endl;
-		cout << endl;
-		t = t->next;
-	}
-}
-void Ttable::XMLout()
-{
-	char *file = "C:/Users/Chris/Desktop/111/%d.XML";
-	char * f = NULL;
-	int i = 1;
-	Node*p = head;
-	while (p != NULL)
-	{
-		f = new char[50];
-		sprintf(f, file, i);
-		CreateXmlFile(p, f);
-		i++;
-		p = p->next;
-	}
-}
+	Node* makeTnode(string& line, string& function, int TYPE);
 
+	Node* exchange(string& line, string& function, int TYPE);
+};
